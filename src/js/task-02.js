886ce0a6f9206bf164d9b8df1a73fda7c5a6495d
Ilya <110407302+flpmustafa@users.noreply.github.com>
2022-12-10 20:58:24 +0200

    Task-2 done

--- src/js/task-02.js
+++ src/js/task-02.js
@@ -6,3 +6,12 @@ const ingredients = [
   'Herbs',
   'Condiments',
 ];
+
+const list = document.querySelector("#ingredients");
+
+let markup ='';
+for(let ingredient of ingredients ) {
+  markup += `<li class="item">${ingredient}</li>`;
+};
+
+list.insertAdjacentHTML("beforeend", markup);
