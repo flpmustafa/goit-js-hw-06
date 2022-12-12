8730c7674c2dc1fb5a4e3d77e2819cb0c78cca3c
Ilya <110407302+flpmustafa@users.noreply.github.com>
2022-12-12 16:14:40 +0200

    Task 09 done

--- js/task-09.js
+++ js/task-09.js
@@ -1,3 +1,11 @@
+const btn = document.querySelector('.change-color');
+
+btn.addEventListener('click', () => {
+  let randomColor = getRandomHexColor();
+  document.body.style.backgroundColor = randomColor;
+  document.querySelector('.color').textContent = randomColor;
+});
+
 function getRandomHexColor() {
   return `#${Math.floor(Math.random() * 16777215).toString(16).padStart(6, 0)}`;
 }
--- src/js/task-07.js
+++
@@ -1,7 +0,0 @@
-const changeRng = document.querySelector('#font-size-control');
-const changeTxt = document.querySelector('#text');
-
-changeRng.addEventListener('change', (event) => {
-    changeTxt.style.fontSize = changeRng.value + 'px'; 
-});
-
