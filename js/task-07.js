6485c96615ad5ef7ad07b9ff27d6f03020654129
Ilya <110407302+flpmustafa@users.noreply.github.com>
2022-12-13 12:09:06 +0200

    tasck 07 is correct

--- js/task-07.js
+++ js/task-07.js
@@ -1,7 +1,7 @@
 const changeRng = document.querySelector('#font-size-control');
 const changeTxt = document.querySelector('#text');
 
-changeRng.addEventListener('change', (event) => {
+changeRng.addEventListener('input', (event) => {
     changeTxt.style.fontSize = changeRng.value + 'px'; 
 });
 
